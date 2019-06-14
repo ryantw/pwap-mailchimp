6bd92e33c968116ed919383b295bd7a0f312d35f
ryan <ryantwalk@gmail.com>
2019-06-14 19:37:09 -0400

    initial commit

---
+++ src/main/java/com/pinwheelsandpearlsboutique/mailchimp/mailchimp/MailchimpApplication.java
@@ -0,0 +1,13 @@
+package com.pinwheelsandpearlsboutique.mailchimp.mailchimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailchimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailchimpApplication.class, args);
+	}
+
+}
